d87227d0661e2594cd33515e392d3696f7c8db5b
ognemrak <oleg.sivalnyov@gmail.com>
2022-03-22 21:21:23 +0200

    Updates HTTP requests

--- Angular-Tour-of-Heroes/src/app/hero-detail/hero-detail.component.ts
+++ Angular-Tour-of-Heroes/src/app/hero-detail/hero-detail.component.ts
@@ -31,4 +31,11 @@ export class HeroDetailComponent implements OnInit {
   public goBack(): void {
     this.location.back();
   }
+
+  public save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
 }
